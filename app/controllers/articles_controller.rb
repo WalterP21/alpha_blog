080e740f06c9894782aa494e031a098ed7112623
Walter Porzel <walter.porzel@thoughtworks.com>
2022-01-25 17:59:15 -0500

    completed index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     #debugger #byebug in rails 6 - look at gemfile to determine
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
 end
